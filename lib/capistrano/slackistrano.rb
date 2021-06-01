bf6835bfe375584b5aae0ecce1df1f2ca668f162
Danil Pismenny <dapi@users.noreply.github.com>
2021-06-01 16:28:08 +0300

    Update slackistrano.rb

--- lib/capistrano/slackistrano.rb
+++ lib/capistrano/slackistrano.rb
@@ -13,7 +13,7 @@ module Slackistrano
     def payload_for_updated
       changelog = fetch(:changelog)
 
-      changelog = changelog.gsub(/\(#(\d+)\)/, "(<%{repo_url}/pull/%{pr}|#%{pr}>)" % {repo_url: fetch(:repo_url), pr: '\1'}) unless changelog.empty?
+      changelog = changelog.gsub(/\(#(\d+)\)/, "(<%{repo_url}/pull/%{pr}|#%{pr}>)" % {repo_url: fetch(:repo_url), pr: '\1'}) unless changelog.to_s.empty?
 
       revision_link = "#{fetch(:repo_url)}/compare/#{fetch(:previous_revision)}...#{fetch(:current_revision)}"
 
